983b5dad3e483cb9a57b748554ee8f662ff4f0cb
Luísa Barros <qlullyq@gmail.com>
2019-10-15 17:18:24 -0300

    feat: configure status bar

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
+/* eslint-disable no-undef */
 import React from 'react';
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {Header} from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-});
-
-console.log("oie");
+import { StatusBar } from 'react-native';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Header />
-        {global.HermesInternal == null ? null : (
-          <View style={styles.engine}>
-            <Text style={styles.footer}>Engine: Hermes</Text>
-          </View>
-        )}
-      </SafeAreaView>
+      <StatusBar barStyle="light-content" backgroundColor="#886cdd" />
     </>
   );
 };
