79138e2ccf846110083f239a25c3ca9f3bae397d
Luísa Barros <qlullyq@gmail.com>
2019-10-16 15:25:56 -0300

    feat: get starred repo from github api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
@@ -8,8 +9,18 @@ export default class User extends Component {
     title: navigation.getParam('user').name,
   });
 
-  async componentDidMount() {
+  state = {
+    stars: [],
+  }
 
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+    console.log(this.state.stars);
   }
 
   render() {
