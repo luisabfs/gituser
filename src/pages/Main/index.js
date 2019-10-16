36eb02163fb23d9f9e48914e5ebba4976b5e6962
Luísa Barros <qlullyq@gmail.com>
2019-10-16 14:07:41 -0300

    feat: add loading icon to button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -21,10 +21,13 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   }
 
   handleSubmit = async () => {
     const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -38,13 +41,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     })
 
     Keyboard.dismiss();
   }
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
     return (
       <Container>
         <Form>
@@ -57,8 +61,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleSubmit}
           />
-          <SubmitButton onPress={this.handleSubmit}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleSubmit}>
+            { loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
 
