6327fb023862e79b74ac2343e89a125e1754fb4b
Luísa Barros <qlullyq@gmail.com>
2019-10-16 00:08:50 -0300

    feat: style navbar

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,22 @@ import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    User
-  })
+  createStackNavigator(
+    {
+      Main,
+      User
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: 'false',
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#F36B7F',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
 );
 
 export default Routes;
