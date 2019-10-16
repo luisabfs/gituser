6fd7a0d8b66322d7ae1375551c223d31ec4a00e5
Luísa Barros <qlullyq@gmail.com>
2019-10-16 15:51:10 -0300

    feat: list starred repos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-import { Container, Header, Avatar, Name, Bio } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -41,6 +53,20 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
       </Container>
     );
   }
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -34,3 +34,45 @@ export const Bio = styled.Text`
   margin-top: 5px;
   text-align: center;
 `;
+
+export const Stars = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Starred = styled.View`
+  background: #f5f5f5;
+  border-radius: 4px;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const OwnerAvatar = styled.Image`
+  width: 42px;
+  height: 42px;
+  border-radius: 21px;
+  background: #eee;
+`;
+
+export const Info = styled.View`
+  margin-left: 10px;
+  flex: 1;
+`;
+
+export const Title = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+  font-size: 15px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const Author = styled.Text`
+  font-size: 13px;
+  color: #666;
+  margin-top: 2px;
+`;
+
