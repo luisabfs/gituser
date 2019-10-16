ff831bec19e5a3eece61f1d8de4c21a969edc1ff
Luísa Barros <qlullyq@gmail.com>
2019-10-16 08:39:23 -0300

    feat: style form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
