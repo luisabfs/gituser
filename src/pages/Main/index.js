b9d0add9867b7e60f72d43e8fb9d5616ba45e64f
Luísa Barros <qlullyq@gmail.com>
2019-10-16 14:30:26 -0300

    feat: save users with async storage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -22,6 +23,22 @@ export default class Main extends Component {
     newUser: '',
     users: [],
     loading: false,
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   handleSubmit = async () => {
